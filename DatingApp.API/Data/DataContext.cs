d3409f2ac777b8bad06e6b8b9e1193774ef1f2fe
Sergio Oliveira <sergio@briisk.co.uk>
2020-05-17 11:12:41 +0200

    End of Sections 8

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -9,9 +9,11 @@ namespace DatingApp.API.Data
 
         public DbSet<User> Users { get; set; }
 
+        public DbSet<Photo> Photos { get; set; }
+
 
         public DataContext(DbContextOptions<DataContext> options)
-            :base(options)
+            : base(options)
         {
         }
 
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -19,6 +19,7 @@ using System.Net;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using DatingApp.API.Helpers;
+using AutoMapper;
 
 namespace DatingApp.API
 {
@@ -36,19 +37,26 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite
             (Configuration.GetConnectionString("DefaultConnection")));
-            
-            services.AddControllers();
+
+            services.AddControllers().AddNewtonsoftJson(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling =
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
             services.AddCors();
-            services.AddScoped<IAuthRepository,AuthRepository>();
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>{
+                .AddJwtBearer(options =>
+                {
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
                                     .GetBytes(Configuration.GetSection("Appsettings:Token").Value)),
                         ValidateIssuer = false,
-                        ValidateAudience = false,            
+                        ValidateAudience = false,
                     };
                 });
         }
@@ -62,27 +70,27 @@ namespace DatingApp.API
             }
             else
             {
-                app.UseExceptionHandler(builder => 
+                app.UseExceptionHandler(builder =>
                 {
                     builder.Run(async context =>
                 {
                     context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
                     var error = context.Features.Get<IExceptionHandlerFeature>();
-                    if(error != null)
+                    if (error != null)
                     {
                         context.Response.AddApplicationError(error.Error.Message);
-                        await context.Response.WriteAsync(error.Error.Message); 
+                        await context.Response.WriteAsync(error.Error.Message);
                     }
                 });
                 });
             }
 
-           // app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseCors(x =>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthentication();
             app.UseAuthorization();
